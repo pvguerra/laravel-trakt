aefabe032f9fa5065f6c46f7c8346498eb624c52
Paulo Guerra <paulovictorguerra@gmail.com>
2022-04-19 21:16:23 -0300

    add: :pen: more trakt users methods

--- src/TraktUser.php
+++ src/TraktUser.php
@@ -69,13 +69,13 @@ class TraktUser extends LaravelTrakt
      *
      * https://trakt.docs.apiary.io/#reference/users/approve-or-deny-follower-requests
      * @param int $requestId
-     * @return array
+     * @return Response
      */
-    public function denyRequest(int $requestId): array
+    public function denyRequest(int $requestId): Response
     {
         $uri = $this->apiUrl . "users/requests/$requestId";
 
-        return Http::withHeaders($this->headers)->withToken($this->apiToken)->delete($uri)->json();
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->delete($uri);
     }
 
     /**
@@ -141,5 +141,231 @@ class TraktUser extends LaravelTrakt
         $uri = $this->apiUrl . "users/$traktId?extended=vip";
 
         return Http::withHeaders($this->headers)->withToken($this->apiToken)->get($uri)->json();
+    }
+
+    /**
+     * Get items a user likes. This will return an array of standard media objects.
+     * You can optionally limit the type of results to return.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/likes
+     * @param string $traktId
+     * @param string $type
+     * @param int $page
+     * @param int $limit
+     * @return array
+     */
+    public function likes(string $traktId, string $type = 'lists', int $page = 1, int $limit = 10): array
+    {
+        $uri = $this->apiUrl . "users/$traktId/likes/$type?page=$page&limit=$limit";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->get($uri)->json();
+    }
+
+    /**
+     * Get all collected items in a user's collection.
+     * A collected item indicates availability to watch digitally or on physical media.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/collection
+     * @param string $traktId
+     * @param string $type
+     * @return array
+     */
+    public function collection(string $traktId, string $type): array
+    {
+        $uri = $this->apiUrl . "users/$traktId/collection/$type?extended=metadata";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->get($uri)->json();
+    }
+
+    /**
+     * Returns all custom lists for a user. Use the /users/:id/lists/:list_id/items method to
+     * get the actual items a specific list contains.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/lists/get-a-user's-custom-lists
+     * @param string $traktId
+     * @return array
+     */
+    public function lists(string $traktId): array
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->get($uri)->json();
+    }
+
+    /**
+     * Create a new custom list. The name is the only required field, but the other info is recommended to ask for.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/lists/create-custom-list
+     * @param string $traktId
+     * @param array $data
+     * @return Response
+     */
+    public function createList(string $traktId, array $data): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->post($uri, $data);
+    }
+
+    /**
+     * Reorder all lists by sending the updated rank of list ids. Use the /users/:id/lists method to get all list ids.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/reorder-lists/reorder-a-user's-lists
+     * @param string $traktId
+     * @param array $data
+     * @return Response
+     */
+    public function reorderLists(string $traktId, array $data): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/reorder";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->post($uri, $data);
+    }
+
+    /**
+     * Returns a single custom list. Use the /users/:id/lists/:list_id/items method to get
+     * the actual items this list contains.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/list/get-custom-list
+     * @param string $traktId
+     * @param string $listId
+     * @return array
+     */
+    public function getList(string $traktId, string $listId): array
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/$listId";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->get($uri)->json();
+    }
+
+    /**
+     * Update a custom list by sending 1 or more parameters. If you update the list name,
+     * the original slug will still be retained so existing references to this list won't break.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/list/update-custom-list
+     * @param string $traktId
+     * @param string $listId
+     * @param array $data
+     * @return Response
+     */
+    public function updateList(string $traktId, string $listId, array $data): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/$listId";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->put($uri, $data);
+    }
+
+    /**
+     * Remove a custom list and all items it contains.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/list/delete-a-user's-custom-list
+     * @param string $traktId
+     * @param string $listId
+     * @return Response
+     */
+    public function deleteList(string $traktId, string $listId): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/$listId";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->delete($uri);
+    }
+
+    /**
+     * Get all items on a custom list. Items can be a movie, show, season, episode, or person.
+     * You can optionally specify the type parameter with a single value or comma delimited string
+     * for multiple item types.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/list-items/get-items-on-a-custom-list
+     * @param int $traktId
+     * @param string $type
+     * @param string $listId
+     * @return array
+     */
+    public function listItems(int $traktId, string $listId, string $type): array
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/$listId/items/$type";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->get($uri)->json();
+    }
+
+    /**
+     * Add one or more items to a custom list. Items can be movies, shows, seasons, episodes, or people.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/list-items/add-items-to-custom-list
+     * @param int $traktId
+     * @param string $listId
+     * @param array $data
+     * @return Response
+     */
+    public function addItemToList(int $traktId, string $listId, array $data): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/$listId/items";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->post($uri, $data);
+    }
+
+    /**
+     * Remove one or more items from a custom list.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/remove-list-items/remove-items-from-custom-list
+     * @param int $traktId
+     * @param string $listId
+     * @param array $data
+     * @return Response
+     */
+    public function removeItemFromList(int $traktId, string $listId, array $data): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/$listId/items/remove";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->post($uri, $data);
+    }
+
+    /**
+     * Reorder all items on a list by sending the updated rank of list item ids.
+     * Use the /users/:id/lists/:list_id/items method to get all list item ids.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/reorder-list-items/reorder-items-on-a-list
+     * @param string $traktId
+     * @param string $listId
+     * @param array $data
+     * @return Response
+     */
+    public function reorderItemsOnList(string $traktId, string $listId, array $data): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/lists/$listId/items/reorder";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->post($uri, $data);
+    }
+
+    /**
+     * If the user has a private profile, the follow request will require approval (approved_at will be null).
+     * If a user is public, they will be followed immediately (approved_at will have a date).
+     * Note: If this user is already being followed, a 409 HTTP status code will returned.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/follow/follow-this-user
+     * @param string $traktId
+     * @return Response
+     */
+    public function follow(string $traktId): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/follow";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->post($uri);
+    }
+
+    /**
+     * If the user has a private profile, the follow request will require approval (approved_at will be null).
+     * If a user is public, they will be followed immediately (approved_at will have a date).
+     * Note: If this user is already being followed, a 409 HTTP status code will returned.
+     *
+     * https://trakt.docs.apiary.io/#reference/users/follow/unfollow-this-user
+     * @param string $traktId
+     * @return Response
+     */
+    public function unfollow(string $traktId): Response
+    {
+        $uri = $this->apiUrl . "users/$traktId/follow";
+
+        return Http::withHeaders($this->headers)->withToken($this->apiToken)->delete($uri);
     }
 }

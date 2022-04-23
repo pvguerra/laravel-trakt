39d3504848b19c44bf662067e2edcda2cc0112e9
Paulo Guerra <paulovictorguerra@gmail.com>
2022-04-23 11:38:30 -0300

    :pencil2: add staging config variables

--- config/trakt.php
+++ config/trakt.php
@@ -3,8 +3,6 @@
 return [
     'api_url' => env('TRAKT_API_URL'),
 
-    'api_url_mock' => env('TRAKT_API_URL_MOCK'),
-
     'client_id' => env('TRAKT_CLIENT_ID'),
 
     'client_secret' => env('TRAKT_CLIENT_SECRET'),
@@ -16,4 +14,16 @@ return [
     ],
 
     'redirect_url' => env('TRAKT_REDIRECT_URL'),
+
+    'staging_api_url' => env('STAGING_TRAKT_API_URL'),
+
+    'staging_client_id' => env('STAGING_TRAKT_CLIENT_ID'),
+
+    'staging_client_secret' => env('STAGING_TRAKT_CLIENT_SECRET'),
+
+    'staging_headers' => [
+        'Content-type' => 'application/json',
+        'trakt-api-version' => env('TRAKT_API_VERSION', '2'),
+        'trakt-api-key' => env('STAGING_TRAKT_CLIENT_ID'),
+    ],
 ];

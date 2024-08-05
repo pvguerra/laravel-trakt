ce1a2529f591ce0d8d900b994807092e95a31980
Ghost <ghost@example.com>
2024-08-05 14:23:49 +0200

    Include ratelimit header in response
    So it can be used later in the application

--- src/Traits/HttpResponses.php
+++ src/Traits/HttpResponses.php
@@ -9,11 +9,22 @@ trait HttpResponses
 {
     protected static function httpResponse(Response $response): JsonResponse
     {
+        $rateLimit = null;
+
+        if (isset($response->headers()['x-ratelimit'])) {
+            try {
+                $rateLimit = json_decode($response->headers()['x-ratelimit'][0]);
+            } catch (\Exception $e) {
+                $rateLimit = null;
+            }
+        }
+
         return response()->json([
             'code' => $response->status(),
             'success' => $response->successful(),
             'client_error' => $response->clientError(),
             'server_error' => $response->serverError(),
+            'rate_limit' => $rateLimit,
             'data' => $response->json(),
         ], $response->status());
     }
